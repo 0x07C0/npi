73dc709c12546ff1f2d233adf102b5028ed3e1b9
Viktor Dudnik <jackfilov@gmail.com>
2022-09-13 02:30:50 +0300

    feat: basic usage

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,79 @@
+#![forbid(unsafe_code)]
+use clap::{CommandFactory, Parser, Subcommand};
+use fsn::parse_file;
+use std::{fs, path::Path, process};
+
+#[derive(Parser)]
+#[clap(name = "New Project Initializer", author, version, about)]
+struct Args {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+
+    /// Display every step
+    #[clap(short, long)]
+    verbose: bool,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Create new directory with a name
+    New {
+        /// Directory name
+        #[clap(value_parser, name = "name")]
+        name: String,
+        /// Project type to be inited
+        #[clap(value_parser, name = "type")]
+        app_type: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let default_conf_dir = format!("{}/.npi",dirs::home_dir().unwrap().to_str().unwrap());
+    match Path::new(&default_conf_dir).try_exists() {
+        Ok(b) => {
+            if !b {
+                match fs::create_dir_all(&default_conf_dir) {
+                    Ok(_) => {}
+                    Err(e) => {
+                        println!("Failed to create directory ({}): {e}", &default_conf_dir)
+                    }
+                }
+            }
+        }
+        Err(e) => println!("Can't read {}: {e}", &default_conf_dir),
+    }
+    let args = Args::parse();
+    match &args.command {
+        Some(Commands::New { name, app_type }) => {
+            println!("Creating project \"{name}\" of type \"{app_type}\"");
+            let full_path = format!(
+                "{}/{}.fsn",
+                &default_conf_dir, &app_type
+            );
+            match parse_file(Path::new(&full_path)) {
+                Ok(s) => {
+                    for dir in s.directories {
+                        match fs::create_dir_all(format!("./{}/{}", &name, &dir)) {
+                            Ok(_) => {}
+                            Err(e) => {
+                                println!("Failed to create directory ({dir}): {e}")
+                            }
+                        }
+                    }
+                    for file in s.files {
+                        let custom_contents = &file.contents.replace(r"{{name}}", &name);
+                        match fs::write(format!("./{}/{}", &name, file.name), custom_contents) {
+                            Ok(_) => {}
+                            Err(e) => println!("Failed to write to file ({}): {e}", file.name),
+                        }
+                    }
+                }
+                Err(e) => println!("Error parsing the file {}: {e}", &full_path),
+            }
+        }
+        None => {
+            _ = Args::command().print_help();
+            process::exit(1);
+        }
+    }
 }
