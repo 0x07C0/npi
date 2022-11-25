0129508ea54cb9beb0ca9e42eee7da00d4c4d7d0
Viktor Dudnik <jackfilov@gmail.com>
2022-11-25 13:45:33 +0200

    refactor: move config dir from fn main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 #![forbid(unsafe_code)]
 use clap::{CommandFactory, Parser, Subcommand};
 use fsn::parse_file;
-use std::{fs, path::Path, process};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    process,
+};
 
 #[derive(Parser)]
 #[clap(name = "New Project Initializer", author, version, about)]
@@ -27,20 +31,32 @@ enum Commands {
     },
 }
 
-fn main() {
+fn get_config_dir() -> Result<PathBuf, String> {
     let default_conf_dir = dirs::config_dir().unwrap().join("npi");
     if !default_conf_dir.exists() {
         if let Err(e) = fs::create_dir_all(&default_conf_dir) {
-            eprintln!(
+            return Err(format!(
                 "Failed to create config directory ({}): {e}",
                 default_conf_dir.to_string_lossy()
-            )
+            ));
         }
     }
+    Ok(default_conf_dir)
+}
+
+fn main() {
+    let conf_dir = match get_config_dir() {
+        Ok(c) => c,
+        Err(e) => {
+            eprintln!("{e}");
+            process::exit(1);
+        }
+    };
+
     let args = Args::parse();
     if let Some(Commands::New { name, app_type }) = &args.command {
         println!("Creating project \"{name}\" of type \"{app_type}\"");
-        let full_path = default_conf_dir.join(format!("{app_type}.fsn"));
+        let full_path = conf_dir.join(format!("{app_type}.fsn"));
         match parse_file(&full_path) {
             Ok(s) => {
                 if let Err(e) = fs::create_dir_all(&name) {
